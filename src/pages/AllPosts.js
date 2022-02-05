317701222c8c3002af045f94260b494e226bf1f8
Barnaby Napier <barnabynapier@gmail.com>
2022-02-05 10:52:10 +0000

    Update styles

--- src/pages/AllPosts.js
+++ src/pages/AllPosts.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Layout from "../components/Layout";
-import PostList from "../components/PostList";
+import { Link } from "react-router-dom";
+import postlist from "../posts.json";
+import "../styles/AllPosts.css";
 
 const AllPosts = () => {
   return (
     <Layout>
-      <PostList />
+      <div className="postlist">
+        {postlist.length &&
+          postlist.map((post, i) => {
+            return (
+              <div key={i} className="post-card">
+                <p>
+                  {" "}
+                  <Link className="link" to={`/post/${post.id}`}>
+                    {post.title}
+                  </Link>
+                  {",  "}
+                  <text className="date">{post.date}</text>
+                </p>
+              </div>
+            );
+          })}
+      </div>
     </Layout>
   );
 };
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
+import "../styles/NotFound.css";
 
 const NotFound = () => {
   return (
     <Layout>
-      <h1 className="notfound">The page you are looking for doesn't exist</h1>
+      <h1 className="notfound">Page not found</h1>
     </Layout>
   );
 };
