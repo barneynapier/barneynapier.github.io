580b27d8c50729146ee1de752a12d24c9a6310c5
Barnaby Napier <barnabynapier@gmail.com>
2022-02-13 11:53:20 +0000

    Remove katex functionality from about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,20 +2,12 @@ import React from "react";
 import pages from "../pages.json";
 import Markdown from "react-markdown";
 import Layout from "../components/Layout";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import "katex/dist/katex.min.css";
 
 export default function AboutPage() {
   return (
     <Layout>
       <div className="page-content">
-        <Markdown
-          linkTarget="_blank"
-          children={pages[0].content}
-          remarkPlugins={[remarkMath]}
-          rehypePlugins={[rehypeKatex]}
-        />
+        <Markdown linkTarget="_blank" children={pages[0].content} />
       </div>
     </Layout>
   );
