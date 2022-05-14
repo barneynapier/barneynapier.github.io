e6a4f68d709f4bd6565120cd3225a1c42050e902
Barnaby Napier <barnabynapier@gmail.com>
2022-05-14 11:58:40 +0100

    Make JSON creation synchronous

--- public/main.js
+++ public/main.js
@@ -1,121 +1,109 @@
 const path = require("path");
 const fs = require("fs");
 
-const dirPath = path.join(__dirname, "../src/content/_posts");
-const dirPathPages = path.join(__dirname, "../src/content/_pages");
-let postlist = [];
-let pagelist = [];
+const postFolder = path.join(__dirname, "../src/content/_posts");
+const pageFolder = path.join(__dirname, "../src/content/_pages");
 
-const getPosts = () => {
-  fs.readdir(dirPath, (err, files) => {
+function getMetadataIndices(acc, elem, i) {
+  if (/^---/.test(elem)) {
+    acc.push(i);
+  }
+  return acc;
+}
+
+function parseMetadata({ lines, metadataIndices }) {
+  let obj = {};
+  if (metadataIndices.length > 0) {
+    let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1]);
+    metadata.forEach((line) => {
+      obj[line.split(": ")[0]] = line.split(": ")[1];
+    });
+    return obj;
+  }
+}
+
+function parseContent({ lines, metadataIndices }) {
+  if (metadataIndices.length > 0) {
+    lines = lines.slice(metadataIndices[1] + 1, lines.length);
+  }
+  return lines.join("\n");
+}
+
+function getPost(file) {
+  let post;
+  try {
+    const contents = fs.readFileSync(`${postFolder}/${file}`, "utf8");
+    const lines = contents.split("\n");
+    const metadataIndices = lines.reduce(getMetadataIndices, []);
+    const metadata = parseMetadata({ lines, metadataIndices });
+    const content = parseContent({ lines, metadataIndices });
+    post = {
+      id: metadata.title.toLowerCase().replace("-", "_"),
+      title: metadata.title ? metadata.title : "No title given",
+      author: metadata.author ? metadata.author : "No author given",
+      date: metadata.date ? metadata.date : "No date given",
+      content: content ? content : "No content given",
+    };
+    return post;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function translatePosts() {
+  let postlist = [];
+  fs.readdir(postFolder, (err, files) => {
     if (err) {
-      return console.log("Failed to list contents of directory: " + err);
-    }
-    files.forEach((file, i) => {
-      let obj = {};
-      let post;
-      fs.readFile(`${dirPath}/${file}`, "utf8", (err, contents) => {
-        const getMetadataIndices = (acc, elem, i) => {
-          if (/^---/.test(elem)) {
-            acc.push(i);
-          }
-          return acc;
-        };
-        const parseMetadata = ({ lines, metadataIndices }) => {
-          if (metadataIndices.length > 0) {
-            let metadata = lines.slice(
-              metadataIndices[0] + 1,
-              metadataIndices[1]
-            );
-            metadata.forEach((line) => {
-              obj[line.split(": ")[0]] = line.split(": ")[1];
-            });
-            return obj;
-          }
-        };
-        const parseContent = ({ lines, metadataIndices }) => {
-          if (metadataIndices.length > 0) {
-            lines = lines.slice(metadataIndices[1] + 1, lines.length);
-          }
-          return lines.join("\n");
-        };
-        const lines = contents.split("\n");
-        const metadataIndices = lines.reduce(getMetadataIndices, []);
-        const metadata = parseMetadata({ lines, metadataIndices });
-        const content = parseContent({ lines, metadataIndices });
-        const date = new Date(metadata.date);
-        const timestamp = date.getTime() / 1000;
-        post = {
-          id: timestamp,
-          title: metadata.title ? metadata.title : "No title given",
-          author: metadata.author ? metadata.author : "No author given",
-          date: metadata.date ? metadata.date : "No date given",
-          content: content ? content : "No content given",
-        };
+      console.log(err);
+    } else {
+      // Get each post
+      files.forEach((file, i) => {
+        let post = getPost(file);
         postlist.push(post);
+        console.log(`Pushed post: ${post.title}`);
+        // Sort by date so posts render in correct order
         if (i === files.length - 1) {
           const sortedList = postlist.sort((a, b) => {
-            return a.id < b.id ? 1 : -1;
+            return a.date < b.date ? 1 : -1;
           });
           let data = JSON.stringify(sortedList);
-          fs.writeFileSync("src/posts.json", data);
+          fs.writeFileSync("../src/posts.json", data);
         }
       });
-    });
-  });
-  return;
-};
-
-const getPages = () => {
-  fs.readdir(dirPathPages, (err, files) => {
-    if (err) {
-      return console.log("Failed to list contents of directory: " + err);
     }
-    files.forEach((file, i) => {
-      let obj = {};
-      let page;
-      fs.readFile(`${dirPathPages}/${file}`, "utf8", (err, contents) => {
-        const getMetadataIndices = (acc, elem, i) => {
-          if (/^---/.test(elem)) {
-            acc.push(i);
-          }
-          return acc;
-        };
-        const parseMetadata = ({ lines, metadataIndices }) => {
-          if (metadataIndices.length > 0) {
-            let metadata = lines.slice(
-              metadataIndices[0] + 1,
-              metadataIndices[1]
-            );
-            metadata.forEach((line) => {
-              obj[line.split(": ")[0]] = line.split(": ")[1];
-            });
-            return obj;
-          }
-        };
-        const parseContent = ({ lines, metadataIndices }) => {
-          if (metadataIndices.length > 0) {
-            lines = lines.slice(metadataIndices[1] + 1, lines.length);
-          }
-          return lines.join("\n");
-        };
-        const lines = contents.split("\n");
-        const metadataIndices = lines.reduce(getMetadataIndices, []);
-        const metadata = parseMetadata({ lines, metadataIndices });
-        const content = parseContent({ lines, metadataIndices });
-        const pageName = metadata.Page;
-        page = {
-          pageName: pageName,
-          content: content,
-        };
-        pagelist.push(page);
-        let data = JSON.stringify(pagelist);
-        fs.writeFileSync("src/pages.json", data);
-      });
-    });
   });
   return;
-};
+}
 
-// getPosts();
-getPages();
+function translatePages() {
+  let pageDict = {};
+  fs.readdir(pageFolder, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // Get each page
+      files.forEach((file, i) => {
+        let page;
+        fs.readFile(`${pageFolder}/${file}`, "utf8", (err, contents) => {
+          const lines = contents.split("\n");
+          const metadataIndices = lines.reduce(getMetadataIndices, []);
+          const metadata = parseMetadata({ lines, metadataIndices });
+          const content = parseContent({ lines, metadataIndices });
+          page = {
+            pageName: metadata.Page,
+            content: content,
+          };
+          pageDict[metadata.Page] = page;
+          console.log(`Pushed page: ${metadata.Page}`);
+          // These two lines shouldnt need to be called in each loop, only need once
+          let data = JSON.stringify(pageDict);
+          fs.writeFileSync("../src/pages.json", data);
+        });
+      });
+    }
+  });
+  return;
+}
+
+translatePosts();
+translatePages();
